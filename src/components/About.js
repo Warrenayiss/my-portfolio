a7748a416e4cf2a04318ff517711a9caf817b937
Warrenayiss <warrenayiss@outlook.com>
2021-03-07 21:59:52 +0000

    Finish About

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import sanityClient from "../client";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import AboutBg from "../AboutImg.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function About() {
-    return (
-        <div>About Page !</div>
-    )
+  const [author, setAuthor] = useState(null);
 
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main className="relative">
+      <img
+        src={AboutBg}
+        alt="Photo by Tran Mau Tri Tam on Unsplash"
+        className="absolute  w-full"
+      />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-purple-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded-md  w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive  text-6xl  text-purple-300 mb-4">
+              Hey There. I'm{" "}
+              <span className="text-purple-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="8eq9o3m4"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
